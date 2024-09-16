14dff666596dbf2addf45f97be1c434571857af8
admin01 <paradoxraphael@gmai.com>
2024-09-16 15:11:51 +1000

    add start files, add global layout using layout.js

--- app/account/page.js
+++ app/account/page.js
@@ -1,9 +1,6 @@
-import Navigation from "../components/Navigation";
-
 function page() {
   return (
     <>
-      <Navigation />
       <h1>Account Page</h1>
     </>
   );
--- app/page.js
+++ app/page.js
@@ -1,10 +1,8 @@
 import Link from "next/link";
-import Navigation from "./components/Navigation";
 
 export default function Page() {
   return (
     <>
-      <Navigation />
       <h1>Hello world</h1>
       <Link href="/cabins">Explore the cabins</Link>
     </>
