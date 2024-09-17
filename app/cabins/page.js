ce3318c4580ddae52ae4b0f2c61952dffee3d70a
admin01 <paradoxraphael@gmai.com>
2024-09-17 10:32:06 +1000

    data fetching in server component

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,5 +1,17 @@
-function page() {
-  return <h1>Cabins Page</h1>;
+async function page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  console.log(data);
+  return (
+    <>
+      <h1>Cabins Page</h1>
+      <ui>
+        {data.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ui>
+    </>
+  );
 }
 
 export default page;
