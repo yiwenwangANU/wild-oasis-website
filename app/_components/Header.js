11a0a0a647a45bbf94da27dbf895679760647508
admin01 <paradoxraphael@gmai.com>
2024-09-18 10:28:58 +1000

    remove starter folder, change import in Header and layout

--- app/_components/Header.js
+++ app/_components/Header.js
@@ -1,9 +1,9 @@
-import Navigation from "@/starter/components/Navigation";
-import Logo from "./Logo";
+import Navigation from "@/app/_components/Navigation";
+import Logo from "@/app/_components/Logo";
 
 function Header() {
   return (
-    <header className="flex place-items-center justify-between px-8 py-5 border-b border-primary-900 max-w-7xl mx-auto">
+    <header className="flex items-center justify-between px-8 py-5 border-b border-primary-900 max-w-7xl mx-auto">
       <Logo />
       <Navigation />
     </header>
--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import "@/app/_styles/globals.css";
+import Header from "@/app/_components/Header";
 import { Josefin_Sans } from "next/font/google";
-import Header from "./_components/Header";
 const josefin = Josefin_Sans({ subsets: ["latin"], display: "swap" });
 
 export const metadata = {
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen`}
       >
         <Header />
         <div className="flex-1 px-8 py-12">
