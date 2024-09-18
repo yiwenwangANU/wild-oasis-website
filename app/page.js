91fe2f88db180dde55cbbbea93b48df2ee4d1883
admin01 <paradoxraphael@gmai.com>
2024-09-18 11:22:53 +1000

    building the home page, with bg image

--- app/page.js
+++ app/page.js
@@ -1,10 +1,27 @@
 import Link from "next/link";
-
+import Image from "next/image";
+import bg from "@/public/bg.png";
 export default function Page() {
   return (
     <>
-      <h1>Hello world</h1>
-      <Link href="/cabins">Explore the cabins</Link>
+      <Image
+        className="object-cover object-top"
+        placeholder="blur"
+        src={bg}
+        fill
+        alt="mountains and forests with two cabins"
+      />
+      <div className="z-10 relative text-center">
+        <h1 className="text-primary-50 text-center text-8xl py-32">
+          Welcome to paradise
+        </h1>
+        <Link
+          className=" text-primary-900  text-center text-lg font-semibold bg-accent-500 px-10 py-7 hover:bg-accent-600 transition-colors"
+          href="/cabins"
+        >
+          Explore luxury cabins
+        </Link>
+      </div>
     </>
   );
 }
