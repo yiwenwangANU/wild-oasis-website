317e70c4f8f0b8f0c78446f6c1c18748f115ec78
admin01 <paradoxraphael@gmai.com>
2024-09-21 16:56:12 +1000

    add country selection and validation to input fiel

--- app/_components/GuestForm.js
+++ app/_components/GuestForm.js
@@ -1,9 +1,16 @@
 "use client";
 import { useForm } from "react-hook-form";
 
+import { useState } from "react";
+import { CountryDropdown } from "react-country-region-selector";
+
 function GuestForm() {
+  const [country, setCountry] = useState("");
+  const [validate, setValidate] = useState(false);
   const {
     register,
+    setValue,
+    trigger,
     handleSubmit,
     formState: { errors },
   } = useForm();
@@ -23,7 +30,7 @@ function GuestForm() {
               ? "bg-accent-100 border-l-8 border-accent-400"
               : "bg-primary-200"
           }`}
-          placeholder={errors["Full name"] && "This field is required."}
+          placeholder={errors["Full name"] ? "This field is required." : ""}
           type="text"
           {...register("Full name", { required: true, maxLength: 80 })}
         />
@@ -37,7 +44,9 @@ function GuestForm() {
               : "bg-primary-200"
           }`}
           placeholder={
-            errors["Email"]?.type === "required" && "This field is required."
+            errors["Email"]?.type === "required"
+              ? "This field is required."
+              : ""
           }
           type="text"
           {...register("Email", { required: true, pattern: /^\S+@\S+$/i })}
@@ -50,21 +59,19 @@ function GuestForm() {
       </div>
       <div>
         <div className="py-3">Where are you from?</div>
-        <select
+
+        <CountryDropdown
           className={`w-full text-primary-800  px-4 py-3 ${
-            errors["Country"]
+            country === "" && validate === true
               ? "bg-accent-100 border-l-8 border-accent-400"
               : "bg-primary-200"
           }`}
-          {...register("Country", { required: true })}
-        >
-          <option value="" disabled selected>
-            Select your Country
-          </option>
-          <option value="Country1">Country1</option>
-          <option value="Country2">Country2</option>
-          <option value="Country3">Country3</option>
-        </select>
+          value={country}
+          onChange={(val) => {
+            setCountry(val);
+            setValue("Country", val, { shouldValidate: true });
+          }}
+        />
       </div>
       <div>
         <div className="py-3">National ID number</div>
@@ -74,7 +81,7 @@ function GuestForm() {
               ? "bg-accent-100 border-l-8 border-accent-400"
               : "bg-primary-200"
           }`}
-          placeholder={errors["National ID"] && "This field is required."}
+          placeholder={errors["National ID"] ? "This field is required." : ""}
           {...register("National ID", { required: true })}
           type="text"
         />
@@ -83,6 +90,9 @@ function GuestForm() {
         <button
           type="submit"
           className=" text-primary-800 text-center text-lg font-semibold bg-accent-500 px-6 py-3 mt-5 hover:bg-accent-600 transition-colors "
+          onClick={() => {
+            setValidate(true);
+          }}
         >
           Update profile
         </button>
