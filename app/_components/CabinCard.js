f9d9983f10da4a55cd6768ec2a4c9a216300bb41
admin01 <paradoxraphael@gmai.com>
2024-09-26 10:16:12 +1000

    update css of cabincard component, use cabin data in the cabincard

--- app/_components/CabinCard.js
+++ app/_components/CabinCard.js
@@ -9,20 +9,28 @@ function CabinCard({ cabin }) {
         <Image src={image} alt="cabin image" fill />
       </div>
       <div className="flex-1 flex flex-col">
-        <div className="basis-1/4 text-2xl px-6 py-4 text-accent-500">
-          Cabin 001
+        <div className="basis-1/4 text-2xl px-6 py-5 text-accent-500">
+          Cabin {name}
         </div>
         <div className="basis-1/4 px-6 flex gap-2">
           <UsersIcon className="h-5 w-5 text-primary-600" />
-          For up to 2 guests
+          For up to {maxCapacity} guests
         </div>
         <div className="flex justify-end">
-          <div className="text-3xl font-sm">$400</div>
-          <div className="p-3">/ night</div>
+          <div className="text-3xl font-sm">${regularPrice - discount}</div>
+          <div className="p-3">
+            {discount ? (
+              <>
+                <s className="text-primary-600">${regularPrice}</s> / night
+              </>
+            ) : (
+              `/night`
+            )}
+          </div>
         </div>
-        <div className="flex ">
+        <div className="flex items-center">
           <div className="basis-1/2"></div>
-          <div className="p-3 flex-1 border-t border-l border-primary-800 place-items-center hover:bg-accent-600 hover:text-primary-950 transition-colors duration-300">
+          <div className="text-center text-sm p-3 flex-1 border-t border-l border-primary-800  hover:bg-accent-600 hover:text-primary-950 transition-colors duration-300">
             Details & Reservation &rarr;
           </div>
         </div>
