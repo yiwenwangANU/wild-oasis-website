8f9c11bcd42c8203fa560f1c51ac9f3fc6ea3beb
admin01 <paradoxraphael@gmai.com>
2024-09-28 14:02:38 +1000

    dynamically generate metadata

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,4 +1,10 @@
 import CabinDetails from "@/app/_components/CabinDetails";
+import { getCabin } from "@/app/_libs/data-service";
+
+export async function generateMetadata({ params }) {
+  const { name } = await getCabin(params.cabinId);
+  return { title: `Cabin ${name}` };
+}
 
 function page({ params }) {
   console.log(params);
