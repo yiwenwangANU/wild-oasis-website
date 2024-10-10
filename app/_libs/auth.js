fd432b91a965864616caba14ae8daf5173b29f5c
admin01 <paradoxraphael@gmai.com>
2024-10-10 16:31:32 +1100

    create new guest when user login with google oauth

--- app/_libs/auth.js
+++ app/_libs/auth.js
@@ -1,13 +1,28 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { createGuest, getGuest } from "@/app/_libs/data-service";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
-  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/login",
+  },
   callbacks: {
     authorized: async ({ auth }) => {
       // Logged in users are authenticated, otherwise redirect to login page
       return !!auth;
+    },
+    async signIn({ user, account, profile }) {
+      try {
+        const existingGuest = await getGuest(user.email);
+        if (!existingGuest) {
+          await createGuest({ email: user.email, fullName: user.name });
+        }
+
+        return true;
+      } catch {
+        return false;
+      }
     },
   },
 });
