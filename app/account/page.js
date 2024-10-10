d86cb3af212c7c2bda3a44c374df14473cc826df
admin01 <paradoxraphael@gmai.com>
2024-10-10 14:26:32 +1100

    set up environment variable and google credential to make auth working on deployment, get user name from session

--- app/account/page.js
+++ app/account/page.js
@@ -1,11 +1,14 @@
+import { auth } from "@/app/_libs/auth";
+
 export const metadata = {
   title: "Account",
 };
-function page() {
+async function page() {
+  const session = await auth();
   return (
     <>
       <h1 className="px-12 py-4 text-2xl text-accent-400 font-semibold">
-        Welcome, Miller
+        Welcome, {session.user?.name?.split(" ")[0]}
       </h1>
     </>
   );
